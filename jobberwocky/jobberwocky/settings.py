8782a86126532b07ff0108fee4a1a547da22a4fb
ricardo <ricardocancar@gmail.com>
2022-01-11 20:50:53 +0100

    remove hard code secret key

--- jobberwocky/jobberwocky/settings.py
+++ jobberwocky/jobberwocky/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from configuration import APP_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-umvkj_peo2y&ezqsy*@**b^&ynhhuv(#g445d%1^osfmrn(*we'
+SECRET_KEY = APP_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
